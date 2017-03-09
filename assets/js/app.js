b35e686717f306bdac11c407cdbfde5e69bdf2d9
Maria <mariacutipa.gonzales@gmail.com>
2017-03-09 16:00:11 -0500

    Login validate

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 var users = [{user:"maria.cg@gmail.com", password: "holaquehace"},
-			 {user:"maria.fe@hotmail.com", password: "Facebook"},
-			 {user:"dianacutipa.gonzales@gmail.com", password: "diana"},
-			 {user:"fernando.acosta@gmail.com", password: "feracosta"}];
+{user:"maria.fe@hotmail.com", password: "Facebook"},
+{user:"dianacutipa.gonzales@gmail.com", password: "diana"},
+{user:"fernando.acosta@gmail.com", password: "feracosta"}];
+
+var usu = document.getElementById('user').value;
+var pwd = document.getElementById('password').value;
 
 function Login() {
-	var usu = document.getElementById('user');
-	var pwd = document.getElementById('password');
+	validateInput();
 
 	for (var i = 0; i < users.length; i++) {
 		if(users[i].user == usu && users[i].password == pwd){
@@ -17,6 +19,18 @@ function Login() {
 
 }
 
-function Redirect(){
+/*function Redirect(){
 	window.location.href = "timeline.html";
+}*/
+
+function validateInput(){
+	var requiredCorreo=document.getElementById('requiredCorreo').value;
+	var requiredPassword=document.getElementById('requiredPassword').value;
+
+	if (usu.length == 0) {
+		requiredCorreo.innerHTML = "El campo de usuario no puede estar en blanco";
+	}
+	if (pwd.length == 0) {
+		requiredPassword.innerHTML = "El campo de usuario no puede estar en blanco";
+	}
 }
